326521fe687ae39813ab5f8103f197c0b4a0155a
Diggo Silva <rdiggosilva@gmail.com>
2025-02-27 13:05:47 -0300

    Implemented navigation on add button to go Add Task Screen

---
+++ ToDoList/App/Scenes/AddTask/AddViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AddViewController.swift
+//  ToDoList
+//
+//  Created by Diggo Silva on 27/02/25.
+//
+
+import Foundation
---
+++ ToDoList/App/Scenes/AddTask/AddViewModel.swift
@@ -0,0 +1,15 @@
+class AddViewModel {
+    var tasks: [TaskModel] = []
+    
+    func validateTask(title: String, completion: @escaping(Result<String, AddTaskError>) -> Void) {
+
+    }
+    
+    func addTask(title: String, completion: @escaping(Result<String, AddTaskError>) -> Void) {
+
+    }
+    
+    func loadTasks() {
+        
+    }
+}
