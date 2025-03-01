de90c648c9dbd88270e4462e2a68ff2724adbfbc
Diggo Silva <rdiggosilva@gmail.com>
2025-03-01 09:32:43 -0300

    Created Repository

--- ToDoList/App/Network/Repository.swift
+++ ToDoList/App/Network/Repository.swift
@@ -1,3 +1,18 @@
+//
+//  Repository.swift
+//  ToDoList
+//
+//  Created by Diggo Silva on 28/02/25.
+//
+
+import Foundation
+
+protocol RepositoryProtocol {
+    func getTasks() -> [TaskModel]
+    func saveTask(_ task: TaskModel, completion: @escaping(Result<String, AddTaskError>) -> Void)
+    func updateTaskModel(task: TaskModel)
+}
+
 class Repository: RepositoryProtocol {
     private let userDefaults = UserDefaults.standard
     private let taskKey = "taskKey"
@@ -16,10 +31,21 @@ class Repository: RepositoryProtocol {
         savedTasks.append(task)
         
         if let encodedTask = try? JSONEncoder().encode(savedTasks) {
-            userDefaults.set(savedTasks, forKey: taskKey)
+            userDefaults.set(encodedTask, forKey: taskKey)
             completion(.success(task.title))
             return
         }
         completion(.failure(.genericError))
+    }
+    
+    func updateTaskModel(task: TaskModel) {
+        let savedTasks = getTasks()
+        
+        if let taskIndex = savedTasks.firstIndex(where: { $0.id == task.id }) {
+            savedTasks[taskIndex].isCompleted = task.isCompleted
+            
+            let encodedTask = try! JSONEncoder().encode(savedTasks) 
+                userDefaults.set(encodedTask, forKey: taskKey)
+        }
     }
 }
